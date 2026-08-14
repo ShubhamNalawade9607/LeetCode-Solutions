e13092c0087e783a4f3b54bcf372c96afa6f68ad
Nalawade Shubham <shubhamnalawade469@gmail.com>
2026-08-14 22:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (94.63%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,12 +1,23 @@
+// class Solution {
+//     public int findComplement(int num) {
+//         int mask=0;
+//         int n=num;
+//         while(n>0){
+//         mask=(mask<<1)|1;
+//         n>>=1;
+//         }
+//         return num^mask;
+//     }
+// }
 class Solution {
     public int findComplement(int num) {
-        int mask=0;
-        int n=num;
-        while(n>0){
-        mask=(mask<<1)|1;
-        n>>=1;
+        int mask = 0;
+
+        for (int temp = num; temp > 0; temp >>= 1) {
+            mask = (mask << 1) | 1;
         }
-        return num^mask;
+
+        return num ^ mask;
     }
 }
             
