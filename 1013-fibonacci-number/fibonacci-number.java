fe472894da861ba5e28144d981ab2d9345d1d5e9
Nalawade Shubham <shubhamnalawade469@gmail.com>
2026-08-16 19:44:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.78%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,23 @@
 class Solution {
     public int fib(int n) {
+        // if(n<=1){
+        //     return n;
+        // }
+        // int fnm1=fib(n-1);
+        // int fnm2=fib(n-2);
+
+        // return fnm1+fnm2;
+
         if(n<=1){
             return n;
         }
-        int fnm1=fib(n-1);
-        int fnm2=fib(n-2);
-
-        return fnm1+fnm2;
+        int a=0;
+        int b=1;
+        for(int i=2; i<=n; i++){
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
     }
 }
