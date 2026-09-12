049624c099049a759310192996bbc68997d3d450
Nalawade Shubham <shubhamnalawade469@gmail.com>
2026-09-12 23:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (28.62%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int si=0;
+        int ei=nums.length-1;
+
+        while(si<=ei){
+            int mid=si+(ei-si)/2;
+
+            if(target==nums[mid]){
+                return true;
+            }
+            //important :Duplicate values → cannot identify sorted half
+            if (nums[si] == nums[mid] && nums[mid] == nums[ei]) {
+                si++;
+                ei--;
+                continue;
+            }
+            if(nums[si]<=nums[mid]){
+                if(nums[si]<=target && target<=nums[mid]){
+                    ei=mid-1;
+                }else{
+                    si=mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && target<=nums[ei]){
+                    si=mid+1;
+                }else{
+                    ei=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
